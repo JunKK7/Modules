f058e244638d676b5b949332b0d812466fe149d9
seouljun <seouljun@innerwave.co.kr>
2024-01-03 16:22:05 +0900

    code converter 수정 (클래스명 재수정 예정)

--- school/src/main/java/com/kj/md/base/convertor/CodeConverter.java
+++ school/src/main/java/com/kj/md/base/convertor/CodeConverter.java
@@ -2,23 +2,30 @@ package com.kj.md.base.convertor;
 
 import com.kj.md.base.code.CommonCode;
 import com.kj.md.base.code.StudentCode;
+import com.kj.md.base.code.StudentCode.GENDER;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import org.apache.commons.lang3.ObjectUtils;
 
 @Converter
-public class CodeConverter<T extends Enum<T> & CommonCode> implements AttributeConverter<T, Integer> {
+public class CodeConverter implements AttributeConverter<GENDER, Integer> {
+
     @Override
-    public Integer convertToDatabaseColumn(T attribute) {
-        return attribute.getCode();
+    public Integer convertToDatabaseColumn(GENDER attribute)
+    {
+
+        return ObjectUtils.isEmpty(attribute.getCode()) ? null : attribute.getCode();
     }
 
     @Override
-    public T convertToEntityAttribute(Integer dbData) {
-        Type type = getClass().getGenericSuperclass();
-        ParameterizedType paramType = (ParameterizedType) type;
-        Class<T> clz = (Class<T>) paramType.getActualTypeArguments()[0];
-        return StudentCode.enumByCode(clz, dbData);
+    public GENDER convertToEntityAttribute(Integer dbData)
+    {
+        if (ObjectUtils.isEmpty(dbData))
+        {
+            return null;
+        }
+        return StudentCode.enumByCode(GENDER.class, dbData);
     }
 }
