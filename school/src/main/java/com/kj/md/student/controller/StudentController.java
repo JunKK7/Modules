f68e54a57f697cb2bf2d855f4213cb0a9b0f9de8
seouljun <seouljun@innerwave.co.kr>
2024-01-03 16:23:32 +0900

    add student controller, service

--- school/src/main/java/com/kj/md/student/controller/StudentController.java
+++ school/src/main/java/com/kj/md/student/controller/StudentController.java
@@ -1,7 +1,35 @@
 package com.kj.md.student.controller;
 
+import com.kj.md.base.domain.student.Student;
+import com.kj.md.student.service.StudentService;
+import java.net.http.HttpResponse;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class StudentController
-{}
+{
+    private final StudentService studentService;
+    @GetMapping("/student")
+    public HttpEntity<List<Student>> get()
+    {
+        List<Student> studentList = studentService.findStudentList();
+        return new HttpEntity<>(studentList);
+    }
+
+    @PostMapping("/student")
+    public HttpEntity<Student> post()
+    {
+        Student save = studentService.save();
+        return new HttpEntity<>(save);
+    }
+
+}
--- school/src/main/java/com/kj/md/student/service/StudentService.java
+++ school/src/main/java/com/kj/md/student/service/StudentService.java
@@ -1,7 +1,33 @@
 package com.kj.md.student.service;
 
+import com.kj.md.base.code.StudentCode.GENDER;
+import com.kj.md.base.domain.student.Student;
+import com.kj.md.base.repository.student.StudentRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class StudentService
-{}
+{
+    private final StudentRepository studentRepository;
+
+    public List<Student> findStudentList()
+    {
+        return studentRepository.findAll();
+    }
+
+    public Student save()
+    {
+        //TEST
+        Student student = new Student();
+
+        student.setStudentName("김경준");
+        student.setAge(31);
+        student.setGender(GENDER.MAN);
+
+        return studentRepository.save(student);
+    }
+}
