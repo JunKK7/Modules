66ef1778154c42cfd4bd1c85b41a25d4538c15da
seouljun <seouljun@innerwave.co.kr>
2024-01-04 16:59:53 +0900

    Converter 이름 변경

--- school/src/main/java/com/kj/md/base/convertor/GenderConverter.java
+++ school/src/main/java/com/kj/md/base/convertor/GenderConverter.java
@@ -1,16 +1,13 @@
 package com.kj.md.base.convertor;
 
-import com.kj.md.base.code.CommonCode;
 import com.kj.md.base.code.StudentCode;
 import com.kj.md.base.code.StudentCode.GENDER;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import org.apache.commons.lang3.ObjectUtils;
 
 @Converter
-public class CodeConverter implements AttributeConverter<GENDER, Integer> {
+public class GenderConverter implements AttributeConverter<GENDER, Integer> {
 
     @Override
     public Integer convertToDatabaseColumn(GENDER attribute)
