ffe747f19065047e6a44b6acde62124123ad3a7a
seouljun <seouljun@innerwave.co.kr>
2024-01-08 11:29:12 +0900

    양방향 관계 생성

--- school/src/main/java/com/kj/md/base/domain/student/Student.java
+++ school/src/main/java/com/kj/md/base/domain/student/Student.java
@@ -3,12 +3,15 @@ package com.kj.md.base.domain.student;
 import com.kj.md.base.code.StudentCode.GENDER;
 import com.kj.md.base.convertor.GenderConverter;
 import com.kj.md.base.domain.BaseDomain;
+import com.kj.md.base.domain.classInformation.ClassInformation;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -31,4 +34,7 @@ public class Student extends BaseDomain
     private GENDER gender;
 
     private Integer age;
+
+    @OneToMany
+    private List<ClassInformation> classInformationList;
 }
